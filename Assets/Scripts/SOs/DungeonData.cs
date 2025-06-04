754dc0f037ba6c358d9ac974f1a2dbdb7e57e321
Ashley N <ewannicholson2004@gmail.com>
2025-06-04 03:42:16 +0100

    Dungeon Foundation
    Added SO for dungeon data

---
+++ Assets/Scripts/SOs/DungeonData.cs
@@ -0,0 +1,46 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Dungeon/Procedural Dungeon Data")]
+public class DungeonData : ScriptableObject
+{
+    [Header("Room Prefabs")]
+    public GameObject startRoomPrefab;
+
+    [Tooltip("A list of rooms used between the start and end.")]
+    public List<RoomPrefabData> roomPrefabs = new List<RoomPrefabData>();
+
+    [Tooltip("Possible end room prefabs, marked as either Exit or Boss.")]
+    public List<EndRoomData> endRoomPrefabs = new List<EndRoomData>();
+
+    [Header("Room Count Scaling")]
+    public int minRoomCount = 3;
+    public int maxRoomCount = 5;
+    public int roomCountGrowthPerFloor = 1;
+
+    [Header("Boss Logic")]
+    [Tooltip("How many floors between bosses. 1 = boss every floor.")]
+    public int floorsPerBoss = 3;
+}
+
+[System.Serializable]
+public class RoomPrefabData
+{
+    public GameObject roomPrefab;
+
+    [Tooltip("Number of doors/connectors in this room.")]
+    public int doorCount;
+}
+
+[System.Serializable]
+public class EndRoomData
+{
+    public GameObject roomPrefab;
+    public EndRoomType roomType;
+}
+
+public enum EndRoomType
+{
+    Exit,
+    Boss
+}
